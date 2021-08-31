12ba17f9582fb49129f47e5f8bd06fdac4f08861
alishaileen <alishaileen05@gmail.com>
2021-08-31 17:48:33 +0700

    added lightenDarkenColor to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,40 @@
 import { API_URL } from '../constants'
 
-export const fetchData = async (url) => {
-    try {
-        const data = await fetch(url)
+export const fetchData = async (endpoint) => {
+  try {
+    const data = await fetch(`${API_URL}${endpoint}`)
 
-        return data
-    } catch (e) {
-        return e
-    }
+    return data
+  } catch (e) {
+    return e
+  }
 
+}
+
+export const lightenDarkenColor = (col, amt) => {
+  var usePound = false;
+
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  var num = parseInt(col,16);
+
+  var r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00FF) + amt;
+
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+
+  var g = (num & 0x0000FF) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
 }
