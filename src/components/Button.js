ac0a5d20004ed0bc66240814dbc127be55dd73c5
alishaileen <alishaileen05@gmail.com>
2021-08-31 17:49:33 +0700

    finished button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { color } from '../constants/style'
+import { lightenDarkenColor } from '../utils'
 
-const StyledButton = styled.div`
-  
-`
+const Button = ({btnText, btnTextColor=color.white, bgColor=color.grey, callbackFunc=()=> {return}}) => {
+  const StyledButton = styled.button`
+    background-color: ${bgColor};
+    color: ${btnTextColor};
+    padding: 0.5rem 2rem;
+    border: none;
+    border-radius: 50px;
+    &:hover {
+      background-color: ${lightenDarkenColor(bgColor, -20)};
+    }
+  `
+  const StyledBtnText = styled.h2`
+    color: ${btnTextColor};
+    margin: 0;
+  `
 
-const Button = () => {
   return (
-    <StyledButton></StyledButton>
+    <StyledButton
+      onClick={() => callbackFunc()}
+    >
+      <StyledBtnText>{btnText}</StyledBtnText>
+    </StyledButton>
   )
 }
 
